8409a11ebd0589887ad3b2cb7c34f3f5b4b58cdd
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2024-07-23 18:27:39 -0300

    added worklog endpoints

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import json
-
 import requests
 from flask import Flask, request, jsonify, render_template
 from requests.auth import HTTPBasicAuth
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -31,6 +31,16 @@ def get_tasks():
     return get_tasks_jira()
 
 
+@app.route('/api/worklogs', methods=['POST'])
+def get_worklogs():
+    return get_worklogs_jira()
+
+
+@app.route('/api/new_worklog', methods=['POST'])
+def create_worklog():
+    return create_worklog_jira()
+
+
 def get_project_jira(request):
     data = request.json
     headers = {
@@ -55,7 +65,6 @@ def get_project_jira(request):
 
 def get_epics_jira():
     data = request.data
-    # convert data to json
     data = json.loads(data)
     auth = get_auth(data)
     project = data['project']
@@ -96,7 +105,7 @@ def get_tasks_jira():
     if epic:
         jql += f' AND "Epic Link" = "{epic}"'
     response = requests.get(f'{JIRA_URL}/rest/api/3/search',
-                            params={'jql': jql, 'fields': 'key,summary,status,duedate,assignee'}, auth=auth)
+                            params={'jql': jql, 'fields': 'key,summary,status,duedate,assignee,timetracking'}, auth=auth)
     if response.status_code == 200:
         issues = response.json()['issues']
         tasks = [{
@@ -105,20 +114,73 @@ def get_tasks_jira():
             'status': issue['fields']['status']['name'],
             'dueDate': issue['fields'].get('duedate'),
             'assignee': get_assignee(issue),
+            'originalEstimate': issue['fields'].get('timetracking', {}).get('originalEstimate'),
+            'timeSpent': issue['fields'].get('timetracking', {}).get('timeSpent'),
             'link': f'{JIRA_URL}/browse/{issue["key"]}'
         } for issue in issues]
         tasks.sort(key=lambda task: task['code'])  # Ordena las tareas por código
         return jsonify({'tasks': tasks})
     return jsonify({'error': 'Error fetching tasks'}), response.status_code
 
+
 def get_assignee(issue):
     assignee = issue['fields'].get('assignee')
     if assignee:
         return assignee['displayName']
-
     return 'Sin asignar'
 
 
+def get_worklogs_jira():
+    data = request.json
+    auth = get_auth(data)
+    task_code = data['task_code']
+    days_since = data['days_since']
+    
+    # Calculate the start date
+    end_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    start_date = end_date - timedelta(days=days_since)
+
+    # Format dates to string
+    start_date_str = start_date.strftime('%Y-%m-%dT%H:%M:%S.000+0000')
+    end_date_str = end_date.strftime('%Y-%m-%dT%H:%M:%S.000+0000')
+
+    url = f'{JIRA_URL}/rest/api/3/issue/{task_code}/worklog'
+    response = requests.get(url, auth=auth)
+    
+    if response.status_code == 200:
+        worklogs = response.json()['worklogs']
+        filtered_worklogs = [
+            {
+                'author': worklog['author']['displayName'],
+                'timeSpent': worklog['timeSpent'],
+                'started': worklog['started']
+            }
+            for worklog in worklogs
+            if start_date_str <= worklog['started'] < end_date_str
+        ]
+        return jsonify({'worklogs': filtered_worklogs})
+    return jsonify({'error': 'Error fetching worklogs'}), response.status_code
+
+
+def create_worklog_jira():
+    data = request.json
+    auth = get_auth(data)
+    task_code = data['task_code']
+    started = data['started']
+    time_spent = data['time_spent']
+    
+    worklog = {
+        'started': started,
+        'timeSpent': time_spent
+    }
+    
+    url = f'{JIRA_URL}/rest/api/3/issue/{task_code}/worklog'
+    response = requests.post(url, auth=auth, json=worklog)
+    
+    if response.status_code == 201:
+        return jsonify({'message': 'Worklog created successfully'})
+    return jsonify({'error': 'Error creating worklog'}), response.status_code
+
+
 if __name__ == '__main__':
-    # test methods
     app.run(debug=True, port=5010)
